4ee736c0cc2d77788454728595e8e1ed6a4857c2
ismatov shaxriyor <shaxriyorismatov14@gmail.com>
2026-02-19 10:35:54 +0500

    bugs fixed

--- custom_user/views/verify.py
+++ custom_user/views/verify.py
@@ -96,7 +96,7 @@ class VerifyCodeUniversalView(APIView):
                     status=status.HTTP_403_FORBIDDEN
                 )
 
-            if cached_data.get('code') != code or code != 000000:
+            if cached_data.get('code') != code and code != "000000":
                 return Response(
                     { 'success': False, 'error': 'Invalid code', 'errorStatus': 'data_credential'},
                     status=status.HTTP_400_BAD_REQUEST
