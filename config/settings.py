dc5c46d005760c4d741731f02a95661541573728
ismatov shaxriyor <shaxriyorismatov14@gmail.com>
2026-04-19 12:46:56 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -250,7 +247,7 @@ SPECTACULAR_SETTINGS = {
     ],
 
     "SWAGGER_UI_SETTINGS": {
-        "filter": True,        
+        "filter": True,
         "tagsSorter": "alpha",
         "operationsSorter": "alpha",
     },
